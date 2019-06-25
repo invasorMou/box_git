873af38f2828b8d61bb672af224af253b25f1120
Mauricio Scioville <invasormou@gmail.com>
2019-06-25 14:54:12 -0500

    test for storing stuff

--- box_spec.rb
+++ box_spec.rb
@@ -12,5 +12,14 @@ describe Box do
     end
   end
 
-  
+  context 'when storing stuffs' do
+    let(:new_box) {Box.new}
+
+    it "should be able to store an object" do
+      expect(new_box.empty?).to be true
+      new_box.content('oranges')
+      expect(new_box.empty?).to be false
+    end
+  end
+
 end
