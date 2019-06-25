751ade4d5388b35bf1685abb5b8183e21f4df47c
Mauricio Scioville <invasormou@gmail.com>
2019-06-25 15:16:53 -0500

    test for information aobut the content

--- box_spec.rb
+++ box_spec.rb
@@ -20,6 +20,18 @@ describe Box do
       new_box.content('oranges')
       expect(new_box.empty?).to be false
     end
+
+    it "should display the stored object information" do
+      expect(new_box.check_box).to be_a String
+      expect(new_box.check_box).to include "object:"
+    end
+
+    it "should display object: String when storing a string" do
+      new_box.content('shoes')
+      expect(new_box.check_box).to include "String"
+    end
   end
+
+
 
 end
