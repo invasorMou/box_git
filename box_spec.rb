b54ac0fd67df4a1a9bb01ecea5029622065cd4bc
Mauricio Scioville <invasormou@gmail.com>
2019-06-25 14:46:07 -0500

    add test to content of the box

--- box_spec.rb
+++ box_spec.rb
@@ -1,8 +1,16 @@
 describe Box do
+
   context 'when created' do
+    let(:new_box) {Box.new}
+
     it 'should be empty' do
-      new_box = Box.new
       expect(new_box.empty?).to be true
     end
+
+    it 'has nil inside' do
+      expect(new_box.content).to eq nil
+    end
   end
+
+  
 end
